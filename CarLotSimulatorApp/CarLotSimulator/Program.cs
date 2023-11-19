091c06204cb2e95bc8b87fddc26ec05b0fb5c9fa
MuscuarBean <email@example.com>
2023-11-19 03:23:48 -0700

    Exercise Complete

--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace CarLotSimulator
 {
@@ -6,6 +7,52 @@ namespace CarLotSimulator
     {
         static void Main(string[] args)
         {
+            var myLot = new CarLot();
+            
+
+
+
+            Console.WriteLine("Car Number One");
+            Car myCar1 = new Car()
+            {
+                Year = 2023,
+                Make = "Toyota",
+                Model = "Camry",
+                HonkNoise = "Loud",
+                EngineNoise = "Low/Medium",
+            };
+
+            myLot.Cars.Add(myCar1);
+
+            Console.WriteLine($"Year: {myCar1.Year}\nMake: {myCar1.Make}\nModel: {myCar1.Model}");
+
+            myCar1.MakeEngineNoise();
+            myCar1.MakeHonkNoise();
+
+            Console.WriteLine("----------------");
+            Console.WriteLine("Car Number Two");
+            Car myCar2 = new Car();
+            myCar2.Year = 2020;
+            myCar2.Make = "Ford";
+            myCar2.Model = "Focus";
+            myCar2.HonkNoise = "Low/Medium";
+            myCar2.EngineNoise = "Low";
+
+            myLot.Cars.Add (myCar2);
+
+            Console.WriteLine($"Year : {myCar2.Year}\nMake: {myCar2.Make}\nModel: {myCar2.Model}");
+            myCar2.MakeEngineNoise();
+            myCar2.MakeHonkNoise();
+
+            Console.WriteLine("----------------");
+
+            Console.WriteLine("Car Number Three");
+
+            var myCar3 = new Car (1969,"Chevy","Camaro");
+            Console.WriteLine($"Year: {myCar3.Year}\nMake: {myCar3.Make}\nModel: {myCar3.Model}");
+
+            myLot.Cars.Add(myCar3 );
+
             //TODO
 
             //Create a seperate class file called Car
@@ -28,6 +75,12 @@ namespace CarLotSimulator
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+            Console.WriteLine("--------------");
+
+            foreach (var car in myLot.Cars)
+            {
+                Console.WriteLine($"Year: {car.Year}Make: {car.Make}Model: {car.Model}");
+            }
         }
     }
 }
